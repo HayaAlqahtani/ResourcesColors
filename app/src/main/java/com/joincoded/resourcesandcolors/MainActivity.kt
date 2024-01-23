7e2b20a24fbf8e27cd9521048253d3ba0ffcfbb2
Haya S A M A Alqahtani <83316@kfh.com>
2024-01-23 10:58:11 +0300

    commit

---
+++ app/src/main/java/com/joincoded/resourcesandcolors/MainActivity.kt
@@ -0,0 +1,209 @@
+package com.joincoded.resourcesandcolors
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.joincoded.resourcesandcolors.ui.theme.ResourcesandcolorsTheme
+
+data class Question(val text: String, val isCorrect: Boolean)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ResourcesandcolorsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TrueFalseGame()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun TrueFalseGame() {
+    var questions by remember { mutableStateOf<List<Question>>(emptyList()) }
+    var currentQuestionIndex = remember { mutableStateOf(0) }
+    var AnsrCorrect by remember { mutableStateOf(false) }
+    var showFeedback by remember { mutableStateOf(false) }
+    var showAnswerOptionsRow by remember { mutableStateOf(true) }
+    var userScore = remember { mutableStateOf(0) }
+    var showResetButton by remember { mutableStateOf(false) }
+
+
+    if (questions.isEmpty()) {
+        questions = listOf(
+            Question(stringResource(R.string.i_work_for_kfh), true),
+            Question(stringResource(R.string.i_am_25_years_old), false),
+            Question(stringResource(R.string.i_have_a_daughter), true),
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = questions[currentQuestionIndex.value].text,
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
+        if (AnsrCorrect && showFeedback ) {
+            AnswerFeedback(stringResource(R.string.correct), MaterialTheme.colorScheme.secondary, true)
+            //CorrectAnswerImage()
+            Text(text = "Score: ${userScore.value}")
+
+            Button(
+                onClick = {
+                    showFeedback = false
+                    showAnswerOptionsRow = true
+                    // Move to the next question or show the final score
+                    if (currentQuestionIndex.value == questions.size - 1) {
+                        showResetButton = true
+
+                        AnsrCorrect = false
+                    }
+
+                    if (currentQuestionIndex.value < questions.size - 1) {
+                        currentQuestionIndex.value++
+
+                    }
+
+
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(10.dp),
+            ) {
+                Text(text = stringResource(R.string.next_questions))
+            }
+        } else if (!AnsrCorrect && showFeedback) {
+            showFeedback = true
+            AnswerFeedback(stringResource(R.string.wrong), MaterialTheme.colorScheme.error, true)
+
+        }
+
+        if (showAnswerOptionsRow) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            )
+            {
+
+                TrueFalseButton("True") {
+                    AnsrCorrect = true == questions[currentQuestionIndex.value].isCorrect
+                    if (AnsrCorrect) {
+                        userScore.value += 1
+                        showAnswerOptionsRow = false
+                    }
+                    showFeedback = true
+
+                }
+                TrueFalseButton("False") {
+                    AnsrCorrect = false == questions[currentQuestionIndex.value].isCorrect
+                    if (AnsrCorrect) {
+                        userScore.value += 1
+                        showAnswerOptionsRow = false
+                    }
+                    showFeedback = true
+
+                }
+            }
+        }
+
+        if (showResetButton) {
+            showAnswerOptionsRow = false
+            Button(onClick = {
+                showAnswerOptionsRow = true
+                showResetButton = false
+
+                currentQuestionIndex.value = 0
+                userScore.value = 0
+                showAnswerOptionsRow = true
+
+
+            }) {
+                Text(text = stringResource(R.string.reset_games))
+            }
+        }
+    }
+}
+
+@Composable
+fun TrueFalseButton(text:  String, showAnswerOptionsRow: () -> Unit) {
+    Button(
+        onClick = {
+            showAnswerOptionsRow()
+
+        },
+        modifier = Modifier
+            .width(120.dp)
+            .height(40.dp)
+
+    ) {
+        Text(text = stringResource(R.string.text))
+    }
+}
+
+@Composable
+fun AnswerFeedback(message: String, backgroundColor: Color, isCorrect: Boolean) {
+    Box(
+        modifier = Modifier
+
+            .size(100.dp)
+            .clip(CircleShape)
+            .clip(MaterialTheme.shapes.large)
+            .background(backgroundColor)
+            .padding(16.dp),
+        Alignment.Center
+
+    ) {
+
+        val imagePainter = if (isCorrect) {
+            painterResource(id = R.drawable.correct_answer)
+        } else {
+            painterResource(id = R.drawable.wrong_answer)
+        }
+
+        Image(
+            painter = imagePainter,
+            contentDescription = if (isCorrect) "Correct Answer Image" else "Wrong Answer Image",
+            modifier = Modifier.fillMaxSize()
+        )
+
+
+            Text(
+                text = message,
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.onPrimary
+
+            )
+        }
+    }
+
+
+@Preview(showBackground = true)
+@Composable
+fun TrueFalseGamePreview() {
+    TrueFalseGame()
+}
